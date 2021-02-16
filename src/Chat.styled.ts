e1964e3b0489365e511b326285d34222f63f58b0
Shinichiro Suzuki <shintrfc@gmail.com>
2021-02-16 22:27:31 +0900

    css done, need code refactoring

--- src/Chat.styled.ts
+++ src/Chat.styled.ts
@@ -1,11 +1,44 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-	background-color: lightGray;
-	width: 17em;
 	margin-bottom: 0.8em;
 	display: flex;
+	align-items: center;
+	.img2 {
+		display: none;
+	}
 	:nth-child(even) {
 		margin-left: auto;
+		flex-direction: row-reverse;
+		.img2 {
+			display: flex;
+		}
+		.img {
+			display: none;
+		}
+	}
+`;
+export const List = styled.div`
+	/* background-color: gainsboro; */
+	background-color: palevioletred;
+	padding: 13px 10px;
+	border-radius: 4px;
+	width: 20em;
+	color: white;
+	font-size: 13px;
+	font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
+		sans-serif;
+	/* margin-bottom: 0.8em; */
+	display: flex;
+	/* margin-right: auto; */
+	/* :nth-child(even) {
+		margin-left: auto;
+	} */
+`;
+
+export const Img = styled.img`
+	display: flex;
+	:nth-child(even) {
+		display: none;
 	}
 `;
--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Container } from "./Chat.styled";
+import { Container, Img, List } from "./Chat.styled";
+import chatBotImg from "./image/chat-bot.png";
+import userIcon from "./image/User Icon.jpeg";
 
 interface Props {
 	conversation: string[];
@@ -8,9 +10,25 @@ interface Props {
 
 const Chat: React.FC<Props> = ({ conversation, isLoading }) => {
 	return (
-		<div>
+		<div style={{ width: "100%" }}>
 			{conversation.map((string) => (
-				<Container key={string}>{string}</Container>
+				<Container key={string}>
+					<Img
+						className='img'
+						src={chatBotImg}
+						alt=''
+						width='50px'
+						height='50px'
+					/>
+					<img
+						className='img2'
+						src={userIcon}
+						alt=''
+						width='50px'
+						height='50px'
+					/>
+					<List>{string}</List>
+				</Container>
 			))}
 			{isLoading ? <p>....</p> : null}
 		</div>
